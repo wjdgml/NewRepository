ff982850a03702bd60c1cf59840dff5d7c8eca37
wjdgml <wjdgml2848@gmail.com>
2023-08-07 23:35:31 +0900

    commit2

--- src/main/java/doto/Apphomework/controller/RecommendationController.java
+++ src/main/java/doto/Apphomework/controller/RecommendationController.java
@@ -8,6 +8,6 @@ public class RecommendationController {
 
     @GetMapping("/")
     public String helloController() {
-        return "To-do Application!";
+        return "To-do Application Test!";
     }
 }
